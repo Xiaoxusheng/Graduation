b07430627611f900069dccd3e5478c44fe59f864
lei <3096407768@qq.com>
2024-02-13 21:07:07 +0800

    初始化

--- servier/main.go
+++ servier/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
+func main() {
+	//初始化配置
+
+	e := gin.Default()
+
+	//中间件
+
+	err := e.Run(":80")
+	if err != nil {
+		log.Println("server start fail")
+		return
+	}
 }
