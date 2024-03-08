679c76b4aecb04846a7d2ce2f29e3f8a26e9bfcc
lei <3096407768@qq.com>
2024-03-08 17:02:15 +0800

    增加公告管理，完善了用户端相关内容
    Signed-off-by: lei <3096407768@qq.com>

--- web/src/views/other/password-strong-page.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <a-card segmented title="密码强度组件">
-    <div class="w-1/3" style="margin: 0 auto">
-      <PasswordStrong/>
-    </div>
-  </a-card>
-</template>
-
-<script lang="ts" setup></script>
